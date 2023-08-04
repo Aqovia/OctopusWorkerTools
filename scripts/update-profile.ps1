ef6509ffb53324825b7b171a89d490b6d3c7d287
Hossein Margani <hossein.margani@aqovia.com>
2023-08-04 08:53:11 +0100

    Temporary commit

---
+++ scripts/update-profile.ps1
@@ -0,0 +1,6 @@
+﻿# Disable colors in PowerShell Core
+$profileDirectory = Split-Path $profile -Parent
+New-Item -ItemType Directory -Force -Path $profileDirectory
+New-Item -Type File -Path $profile
+Add-Content -Path $profile -Value '$PSStyle.OutputRendering="PlainText"'
+. $profile;
