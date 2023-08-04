433a7c52f8e046ecd0905582305af71bc8571eda
Hossein Margani <hossein.margani@aqovia.com>
2023-08-04 11:23:27 +0100

    DO-4: Use scripts folder for all images

--- windows-ltsc2019-node18/scripts/update_path.cmd
+++
@@ -1,1 +0,0 @@
-setx /M path "%PATH%;C:\Users\ContainerAdministrator\AppData\Local\Microsoft\dotnet;C:\Program Files\PackageManagement\NuGet\Packages\Octopus.Client.11.6.3644\lib\net452\Octopus.Client.dll"
