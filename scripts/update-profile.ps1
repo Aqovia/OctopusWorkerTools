e5add1ed0585bfb1563b0164272758a1b8a2f822
Hossein Margani <hossein.margani@aqovia.com>
2023-08-06 08:24:09 +0100

    DO-4: Add term environment variable

--- scripts/update-profile.ps1
+++ scripts/update-profile.ps1
@@ -1,4 +1,8 @@
-﻿# Disable colors in PowerShell Core
+﻿# Disable colors using TERM environment variable
+# https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_ansi_terminals?view=powershell-7.3#disabling-ansi-output
+[System.Environment]::SetEnvironmentVariable('TERM','dumb', 'Machine')
+
+# Disable colors in PowerShell Core
 $profileDirectory = Split-Path $profile -Parent
 New-Item -ItemType Directory -Force -Path $profileDirectory
 New-Item -Type File -Path $profile
